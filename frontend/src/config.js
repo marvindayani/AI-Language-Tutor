0788363a98c5aa87844f950624f676f1debdf748
Marvin Dayani <marvinpdayani@gmail.com>
2026-04-17 09:25:23 +0530

    feat: add configuration file for backend API base URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5000';
-// const BASE_URL = 'https://ai-language-hdu0.onrender.com';
+// const BASE_URL = 'http://localhost:5000';
+
+const BASE_URL = 'https://ai-language-hdu0.onrender.com';
 
 export default BASE_URL;
